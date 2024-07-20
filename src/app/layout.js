4fe1b9724bed64309f233953eeccbae7b4f20473
AgustioMaitimu <agustiomaitimu8@gmail.com>
2024-07-21 04:57:04 +0700

    feat: added site title

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import './globals.css';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <title>Agustio Maitimu | Front-End Developer Portfolio</title>
       <body className="z-50 bg-[#16191D]">{children}</body>
     </html>
   );
